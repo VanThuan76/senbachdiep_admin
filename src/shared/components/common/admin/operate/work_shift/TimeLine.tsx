be1270e83fcd4bae064aa2d7887b7124622ea78a
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-01-13 14:46:29 +0700

    thuanvv/fix-cal-timeline

--- src/shared/components/common/admin/operate/work_shift/TimeLine.tsx
+++ src/shared/components/common/admin/operate/work_shift/TimeLine.tsx
@@ -36,6 +36,8 @@ const fakeData = {
   },
 };
 const TimeLineWorkShift = () => {
+  let accumulatedWidth = 0;
+  const widths: any[] = [];
   return (
     <div className='grid w-full grid-cols-1 items-start justify-between gap-8 overflow-hidden md:grid-cols-23'>
       <div className='w-full border border-[#DFD24C] p-4 md:col-span-5'></div>
@@ -43,15 +45,15 @@ const TimeLineWorkShift = () => {
         <div className='mt-4 flex flex-col items-start justify-start gap-8'>
           <div className='ml-20 flex h-full w-[1206px] items-center justify-start'>
             {timeLineWorkShifts.map(time => (
-              <p key={time} className='!w-[67px]'>
+              <p key={time} className='!w-[67px] text-center'>
                 {time}
               </p>
             ))}
           </div>
           {fakeData.data.zone.single.map((item: { [key: string]: any }, index: number) => (
-            <div key={index} className='border-b-bg-[#FCFCFC] flex w-full items-center justify-start border-b'>
+            <div key={index} className='border-b-bg-[#FCFCFC] min-w-screen flex items-center justify-start border-b'>
               <div className='min-w-[60px] rounded-md bg-[#D9A536] px-2 py-4 text-center'>D0{index + 1}</div>
-              <div className='ml-5 flex w-full items-center justify-start gap-2 overflow-hidden'>
+              <div className='min-w-screen ml-5 flex items-center justify-start gap-2 overflow-hidden'>
                 {(
                   (item[`room_${index + 1}`] as {
                     id: number;
@@ -60,14 +62,20 @@ const TimeLineWorkShift = () => {
                     to_at: string;
                     status: number;
                   }[]) || []
-                ).map((child, childIndex) => (
-                  <div
-                    key={childIndex}
-                    className={cn('rounded-md border-2 border-solid border-[#C9C9C9] px-2 py-4 text-center', `w-full`)}
-                  >
-                    {child.id} - {child.staff} ({child.from_at} - {child.to_at})
-                  </div>
-                ))}
+                ).map((child, childIndex) => {
+                  const dynamicWidth = 60 * calculationTimeLine(child.to_at) - accumulatedWidth;
+                  accumulatedWidth += dynamicWidth;
+                  widths.push(dynamicWidth);
+                  return (
+                    <div
+                      key={childIndex}
+                      className={cn('rounded-md border-2 border-solid border-[#C9C9C9] px-2 py-4 text-center')}
+                      style={{ width: dynamicWidth + 'px' }}
+                    >
+                      {child.id} - {child.staff} ({child.from_at} - {child.to_at})
+                    </div>
+                  );
+                })}
               </div>
             </div>
           ))}
